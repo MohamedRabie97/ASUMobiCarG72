5cb461328a1b77456e0ab108ca98a4ff6bd4960b
MariaMounir97 <36592201+MariaMounir97@users.noreply.github.com>
2018-02-22 20:50:23 +0200

    Add files via upload

---
+++ IntroPage/sheet_1_Q2.ino
@@ -0,0 +1,41 @@
+int r=4;
+int b=5;
+int y=6;
+int w=7;
+void setup() {
+
+              pinMode(r,OUTPUT);
+              pinMode(b,OUTPUT);
+              pinMode(y,OUTPUT);
+              pinMode(w,OUTPUT);
+}
+
+void loop() {
+
+             digitalWrite(r,HIGH);
+             delay(1000);
+             digitalWrite(r,LOW);
+             digitalWrite(b,HIGH);
+             delay(1000);
+             digitalWrite(b,LOW);
+             digitalWrite(y,HIGH);
+             delay(1000);
+             digitalWrite(y,LOW);
+             digitalWrite(w,HIGH);
+             delay(1000);
+             digitalWrite(w,LOW);
+             delay(1000);
+             digitalWrite(w,HIGH);
+             delay(1000);
+             digitalWrite(w,LOW);
+             digitalWrite(y,HIGH);
+             delay(1000);
+             digitalWrite(y,LOW);
+             digitalWrite(b,HIGH);
+             delay(1000);
+             digitalWrite(b,LOW);
+             digitalWrite(r,HIGH);
+             delay(1000);
+             digitalWrite(r,LOW);
+             delay(1000);
+}
