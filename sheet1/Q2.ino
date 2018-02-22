1cf5f3acd3b5e9bc8b3962d9153cacc760c69aec
MarinaMicheal <36561626+MarinaMicheal@users.noreply.github.com>
2018-02-22 21:10:23 +0200

    Add files via upload

---
+++ sheet1/Q2.ino
@@ -0,0 +1,43 @@
+int led1=4;
+int led2=5;
+int led3=6;
+int led4=7;
+void setup() {
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  pinMode(led4,OUTPUT);
+
+}
+
+void loop() {
+ digitalWrite(led4,HIGH);
+ delay(1000);
+ digitalWrite(led4,LOW);
+ digitalWrite(led3,HIGH);
+delay(1000);
+ digitalWrite(led3,LOW);
+ digitalWrite(led2,HIGH);
+delay(1000);
+ digitalWrite(led2,LOW);
+ digitalWrite(led1,HIGH);
+ delay(1000);
+digitalWrite(led1,LOW);
+delay(1000);
+digitalWrite(led1,HIGH);
+delay(1000);
+digitalWrite(led1,LOW);
+digitalWrite(led2,HIGH);
+  delay(1000);
+ digitalWrite(led2,LOW);
+ digitalWrite(led3,HIGH);
+ delay(1000);
+ digitalWrite(led3,LOW);
+ digitalWrite(led4,HIGH);
+ delay(1000);
+ digitalWrite(led4,LOW);
+}
+ 
+ 
+
+
