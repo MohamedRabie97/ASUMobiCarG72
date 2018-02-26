421137a7fa1bc2faafe31c073f1748fa7342033c
MohamedRabie97 <36548647+MohamedRabie97@users.noreply.github.com>
2018-02-26 23:57:42 +0200

    Add files via upload

---
+++ Example_2.ino
@@ -0,0 +1,23 @@
+const int LEDpin = 13;
+const int buttonPin = 2;
+int val;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LEDpin,OUTPUT);
+  pinMode(buttonPin,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = digitalRead(buttonPin);
+  if(val == HIGH)
+  {
+    digitalWrite(LEDpin,HIGH);
+      delay(1000);
+      digitalWrite(LEDpin,LOW);
+      delay(1000);
+  }
+  else {digitalWrite(LEDpin,LOW);}
+
+}
