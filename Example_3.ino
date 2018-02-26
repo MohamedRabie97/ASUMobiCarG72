b35b09d85491bf354ec16a1b30d2440cb2ff66c6
MohamedRabie97 <36548647+MohamedRabie97@users.noreply.github.com>
2018-02-27 00:29:35 +0200

    Example 3 by push buttons

---
+++ Example_3.ino
@@ -0,0 +1,19 @@
+const int ledpin = 13;
+const int inputpin1 = 2;
+const int inputpin2 = 3;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledpin,OUTPUT);
+  pinMode(inputpin1,INPUT);
+  pinMode(inputpin2,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(inputpin1)==HIGH)
+  {digitalWrite(ledpin,LOW);}
+  else if(digitalRead(inputpin2)==HIGH)
+  {digitalWrite(ledpin,HIGH);}
+
+}
