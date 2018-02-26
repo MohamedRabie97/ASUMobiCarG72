07b77c6de0dfd8d81a8e2af5388382e410eaedf3
MohamedRabie97 <36548647+MohamedRabie97@users.noreply.github.com>
2018-02-26 23:20:43 +0200

    Add files via upload

---
+++ sketch_feb24a.ino
@@ -0,0 +1,15 @@
+const int LED = 13;
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(LED,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED,HIGH);
+  delay(1000);
+  digitalWrite(LED,LOW);
+  delay(1000);
+
+}
