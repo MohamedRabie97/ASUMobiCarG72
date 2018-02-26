69ad9e7bacebef516ffeb743db69bdf740fca366
MohamedRabie97 <36548647+MohamedRabie97@users.noreply.github.com>
2018-02-27 00:39:44 +0200

    sheet1_Question1

---
+++ Sheet1_Q1.ino
@@ -0,0 +1,14 @@
+const int input_1 = 13;
+void setup() {
+  
+pinMode(input_1,OUTPUT);
+}
+
+void loop() {
+ digitalWrite(input_1,HIGH);
+ delay(500);
+ digitalWrite(input_1,LOW);
+ delay(1500);
+  
+
+}
