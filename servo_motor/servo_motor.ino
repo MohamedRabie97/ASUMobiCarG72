8907c0f176f2f2f84867d288c0c6f959ba04a1a0
marina-fares <36383830+marina-fares@users.noreply.github.com>
2018-02-27 23:35:03 +0200

    Add files via upload

---
+++ servo_motor/servo_motor.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+Servo servo_1;
+void setup() {
+  // put your setup code here, to run once:
+servo_1.attach(6);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for(int i=0;i<255;i++){
+ servo_1.write(i);
+ delay(20);
+  }
+  for(int i=255;i>0;i--){
+   servo_1.write(i);
+   delay(20);
+    }
+}
