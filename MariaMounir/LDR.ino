ddb8850d821a04a44365b694e26a1a0b51785e9b
MariaMounir97 <36592201+MariaMounir97@users.noreply.github.com>
2018-02-28 14:46:40 +0200

    Add files via upload

---
+++ MariaMounir/LDR.ino
@@ -0,0 +1,20 @@
+int led=11;
+int ldr=A0;
+int level;
+void setup() {
+              Serial.begin(9600);
+              pinMode(led,OUTPUT);
+              pinMode(ldr,INPUT);
+}
+
+void loop() {
+              level=analogRead(ldr);
+              
+              level=map(level,0,1023,0,255);
+              
+              Serial.println(level);
+              
+              analogWrite(led,level);
+              
+              delay(200);
+}
