eb80546ac94f7518024f1e5ddb3a4d9e012796be
MariaMounir97 <36592201+MariaMounir97@users.noreply.github.com>
2018-03-04 02:09:45 +0200

    Add files via upload

---
+++ MariaMounir/var_res.ino
@@ -0,0 +1,19 @@
+int var_res=A4;
+int led=3;
+int value;
+void setup() {
+               pinMode(led,OUTPUT);
+               
+               serial.begin(9600);    
+}
+
+void loop() {
+             value=analogRead(var_res);
+             
+             value=map(value,0,1023,0,255);
+
+             serial.println(value);
+                          
+             analogWrite(led,value);
+             
+}
