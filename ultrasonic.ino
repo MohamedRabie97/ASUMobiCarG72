c366c2ca04cfd9fa38976bef16c255202cd4261f
MariaMounir97 <36592201+MariaMounir97@users.noreply.github.com>
2018-03-28 15:07:53 +0200

    Add files via upload

---
+++ ultrasonic.ino
@@ -0,0 +1,31 @@
+#define echoPin 11
+#define trigPin 12
+long distance, duration;
+
+void setup() {
+               
+               pinMode(echoPin,INPUT);
+               
+               pinMode(trigPin,OUTPUT);
+}
+
+void loop() {
+              digitalWrite(trigPin,LOW);
+              delayMicroseconds(2);
+              
+              digitalWrite(trigPin,HIGH);
+              delayMicroseconds(10);
+              
+              digitalWrite(trigPin,LOW);
+
+              duration = pulseIn(echoPin,HIGH);
+
+              distance = (duration/2) * 0.034029;
+
+              if(distance < 200){
+                                 stop();     //this instruction will stop the car 
+                }
+              
+
+              
+}
