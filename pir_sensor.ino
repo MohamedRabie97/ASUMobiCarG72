ba655892763c0cf9f21ded334a2517bbdcb7edbd
MariaMounir97 <36592201+MariaMounir97@users.noreply.github.com>
2018-03-28 17:07:26 +0200

    Add files via upload

---
+++ pir_sensor.ino
@@ -0,0 +1,22 @@
+int pirpin=2;
+int ledpin=6;
+int buzzer=11;
+void setup() {
+              pinMode(pirpin,INPUT);
+              pinMode(ledpin,OUTPUT);
+              pinMode(buzzer,OUTPUT);
+}
+
+void loop() {
+             int pir_val = digitalRead(pirpin);
+
+             if (pir_val == HIGH){
+                                   digitalWrite(ledpin,LOW);
+                                   digitalWrite(buzzer,HIGH);                  
+                                 }
+            
+             if (pir_val == LOW){
+                                   digitalWrite(ledpin,HIGH);                 
+                                   digitalWrite(buzzer,LOW);                                
+                                }                    
+}
