142b5575754a1e254e6b2fe5978c147b2314905a
MariaMounir97 <36592201+MariaMounir97@users.noreply.github.com>
2018-03-28 15:21:39 +0200

    Add files via upload

---
+++ Tilt_sensor.ino
@@ -0,0 +1,19 @@
+#define tilt 8
+int led=2;
+void setup() {
+              pinMode(tilt,INPUT);
+              pinMode(led,OUTPUT);
+              serial.begin(9600);
+
+}
+
+void loop() {
+             int out = digitalRead(8);
+
+             serial.println(out);
+
+             delay(100);
+
+             if(out==HIGH){digitalWrite(led,HIGH);}
+             else{digitalWrite(led,LOW);}
+}
